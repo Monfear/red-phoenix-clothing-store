923108c8d07a461eb3727f73aca315029c5fbab2
Monfear <wnuq@ymail.com>
2021-11-20 10:04:43 +0100

    create error page

--- src/Components/Pages/Error/Error.jsx
+++ src/Components/Pages/Error/Error.jsx
@@ -1,9 +1,13 @@
 import styles from "./Error.module.css";
 
+import { Link } from "react-router-dom";
+
 export const Error = () => {
     return (
-        <div>
-            <h1>Error</h1>
-        </div>
+        <section className={styles.error}>
+            <Link to={"/"}>
+                <button className={styles.backBtn}>back to home</button>
+            </Link>
+        </section>
     );
 };
